37c8e83611ca61caca8b9712bfcdffda9a881331
THE_COLLECTOR <cdmx2125@amerike.edu.mx>
2023-10-16 09:02:07 -0600

    spawn

--- Assets/Scripts/spawn.cs
+++ Assets/Scripts/spawn.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
+    public float speed = 10.0f;
+    private Rigidbody2D RB;
+    private Vector2 screenBounds;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = this.GetComponent<Rigidbody2D>();
+        RB.velocity = new Vector2(-speed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
+
+  
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < screenBounds.x * 2)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
