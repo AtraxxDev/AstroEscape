3e4b7fc54453ac15612b9e0e4bd9c5a933c38bac
vicio <cdmx2115@amerike.edu.mx>
2023-10-18 18:04:23 -0600

    Codigos JJJV pulidos

--- Assets/Scripts/Oxygen.cs
+++ Assets/Scripts/Oxygen.cs
@@ -8,6 +8,8 @@ public class Oxygen : MonoBehaviour
     public Image timer_linear_image;
     float time_remaining;
     public float max_time = 120f;
+    public float oxygenRecovery = 15f;
+    ShipHealth sh;
 
     void Start()
     {
@@ -20,6 +22,23 @@ public class Oxygen : MonoBehaviour
         {
             time_remaining -= Time.deltaTime;
             timer_linear_image.fillAmount = time_remaining / max_time;
+        }
+        if (time_remaining > 0 && sh.healthAmount <= 4f)
+        {
+            time_remaining -= Time.deltaTime * 2;
+            timer_linear_image.fillAmount = time_remaining / max_time;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("oxigeno"))
+        {
+            time_remaining += oxygenRecovery;
+            if (time_remaining > max_time)
+            {
+                time_remaining = max_time;
+            }
         }
     }
 }
