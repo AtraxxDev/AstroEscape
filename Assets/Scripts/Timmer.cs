4f170cfead6275dcf9127a6ad791f36654a670d7
ChristopherOrtiz69 <christopher.villafanet@gmail.com>
2023-10-19 18:11:18 -0600

    Arreglando timmer

--- Assets/Scripts/Timmer.cs
+++ Assets/Scripts/Timmer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timmer : MonoBehaviour
 {
@@ -10,11 +11,12 @@ public class Timmer : MonoBehaviour
 
     private bool isRunning = false;
 
-    public Text timerText; // Agrega una referencia al elemento de interfaz de usuario.
+    public TMP_Text timerText; // Agrega una referencia al elemento de interfaz de usuario.
 
     private void Start()
     {
         currentTime = 0; // Comienza desde cero.
+        StartTimer();
 
         // Asegúrate de que timerText esté asignado en el Inspector.
         if (timerText == null)
@@ -28,34 +30,37 @@ public class Timmer : MonoBehaviour
         if (isRunning)
         {
             currentTime += Time.deltaTime; // Suma tiempo en lugar de restarlo.
-            UpdateTimerText(); // Llama a la función para actualizar el texto del temporizador.
+            UpdateTimerText(); // Llama a la función para actualizar el texto del temporizador.  
         }
+        
     }
-
     public void StartTimer()
     {
         isRunning = true;
     }
-
     public void StopTimer()
     {
         isRunning = false;
     }
-
     public void ResetTimer()
     {
         currentTime = 0; // Reinicia el temporizador a cero.
         isRunning = false;
         UpdateTimerText(); // Asegúrate de actualizar el texto al reiniciar el temporizador.
     }
-
-    private void UpdateTimerText()
+    public void GuardarTime() // Aqui se guardara el tiempo 
+    {
+        Debug.Log(currentTime); //Se guarda el tiempo
+    }
+    public  void UpdateTimerText()
     {
         if (timerText != null)
         {
             timerText.text = "Tiempo: " + currentTime.ToString("F2"); // Muestra el tiempo con dos decimales.
+            Debug.Log("tiempo");
         }
     }
+
 }
 
 
