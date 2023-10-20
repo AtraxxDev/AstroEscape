6a1ffbebf9835e23062ec27f23684d1f3d456080
cdmx2125@amerike.edu.mx <100314911+DomnhallIvan@users.noreply.github.com>
2023-10-19 19:00:45 -0600

    random spawn y vel

--- Assets/Scripts/movimientoAsteroides.cs
+++ Assets/Scripts/movimientoAsteroides.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class movimientoAsteroides : MonoBehaviour
 {
+    public GameObject des;
+    public float tiempoDes;
     public float vel;
     void Update()
     {
+        
         transform.position += Vector3.right * vel * Time.deltaTime;
+
+        Destroy(des, tiempoDes);
+        
     }
 }
--- Assets/Scripts/spawn.cs
+++ Assets/Scripts/spawn.cs
@@ -7,7 +7,9 @@ public class spawn : MonoBehaviour
     public GameObject objetoPrefab; 
     public float minY = 1.0f;
     public float maxY = 5.0f; 
-    public float tiempoDeSpawn = 2.0f; 
+    public float tiempoDeSpawn;
+    public float maxTime;
+    public float minTime;
 
     private float tiempoTranscurrido = 0.0f;
 
@@ -19,6 +21,8 @@ public class spawn : MonoBehaviour
         {
             SpawnObject();
             tiempoTranscurrido = 0.0f;
+            tiempoDeSpawn = Random.Range(minTime, maxTime);
+
         }
     }
 
@@ -31,4 +35,7 @@ public class spawn : MonoBehaviour
         Vector3 spawnPosition = new Vector3(transform.position.x, randomY, transform.position.z);
         Instantiate(objetoPrefab, spawnPosition, Quaternion.identity);
     }
+
+
+
 }
