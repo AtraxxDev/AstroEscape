9bf7e481b4f080fe8c90f2f38913beabf060a1cf
MaxIb <cdmx2254@amerike.edu.mx>
2023-11-09 17:37:21 -0600

    zoom out corregido

--- Assets/Scripts/ZoomOutCam.cs
+++ Assets/Scripts/ZoomOutCam.cs
@@ -5,25 +5,53 @@ using UnityEngine;
 
 public class ZoomOutCam : MonoBehaviour
 {
-        public Transform target;
-        public float zoomSpeed = 2.0f;
-        public float minZoom = 10.0f;
-        public float maxZoom = 50.0f;
+    public string targetTag = ""; 
+    public float zoomSpeed = 2.0f;
+    public float minZoom = 10.0f;
+    public float maxZoom = 50.0f;
 
-        public Camera cam;
+    public Camera cam;
 
-        void Start()
-        {
-           // cam = GetComponent<Camera>();
-        }
-
-        void Update()
-        {
-            float distance = Vector3.Distance( target.position,transform.position);
-            float zoom = Mathf.Clamp(distance, maxZoom, minZoom);
-            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoom, Time.deltaTime * zoomSpeed);
+    void Start()
+    {
         
+    }
+
+    void Update()
+    {
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(targetTag);
+
+        if (objectsWithTag.Length > 0)
+        {
+            Transform nearestObject = objectsWithTag[0].transform;
+            float nearestDistance = Vector3.Distance(nearestObject.position, transform.position);
+
+            foreach (GameObject obj in objectsWithTag)
+            {
+                float distance = Vector3.Distance(obj.transform.position, transform.position);
+                if (distance < nearestDistance)
+                {
+                    nearestObject = obj.transform;
+                    nearestDistance = distance;
+                }
+            }
+
+            float zoom = Mathf.Clamp(nearestDistance,maxZoom, minZoom);
+
+            if (nearestDistance < minZoom)
+            {
+                
+                Debug.Log("maxZoom");
+            }
+            else if (nearestDistance > maxZoom)
+            {
+                
+                Debug.Log("minZoom");
+            }
+
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoom, Time.deltaTime * zoomSpeed);
         }
+    }
 }
 
 
