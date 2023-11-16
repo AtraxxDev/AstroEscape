b2506f2ebbafc445399828b60a3e3d5005d0eb1f
Elliot Reyes <gametraxworks@gmail.com>
2023-11-15 23:53:45 -0600

    Agregando Nuevo Parallax Y Inicio de new level 1

--- Assets/Scripts/ZoomOutCam.cs
+++ Assets/Scripts/ZoomOutCam.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ZoomOutCam : MonoBehaviour
@@ -8,61 +6,39 @@ public class ZoomOutCam : MonoBehaviour
     public float zoomSpeed = 2.0f;
     public float minZoom = 10.0f;
     public float maxZoom = 50.0f;
+    public float zoomInDelay = 1.0f; // Ajusta el retraso al salir del trigger
     public Camera cam;
 
     private bool isInTrigger = false;
-
-    void Start()
-    {
-        // No hay cambios en el método Start
-    }
+    private float timeSinceExitTrigger = 0f;
 
     void Update()
     {
         if (isInTrigger)
         {
-            // Encuentra todos los objetos en la escena con la etiqueta especificada
-            GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(targetTag);
+            // Realiza el zoom out si el jugador está dentro del collider
+            float zoom = Mathf.Clamp(maxZoom, minZoom, maxZoom);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoom, Time.deltaTime * zoomSpeed);
 
-            // Si hay al menos un objeto con la etiqueta especificada
-            if (objectsWithTag.Length > 0)
+            // Restablece el temporizador de salida del trigger
+            timeSinceExitTrigger = 0f;
+        }
+        else
+        {
+            // Incrementa el temporizador de salida del trigger
+            timeSinceExitTrigger += Time.deltaTime;
+
+            // Si ha pasado el tiempo de retraso, realiza el zoom in
+            if (timeSinceExitTrigger >= zoomInDelay)
             {
-                // Inicializa el objeto más cercano y su distancia al primer objeto de la lista
-                Transform nearestObject = objectsWithTag[0].transform;
-                float nearestDistance = Vector3.Distance(nearestObject.position, transform.position);
-
-                // Itera sobre todos los objetos para encontrar el más cercano
-                foreach (GameObject obj in objectsWithTag)
-                {
-                    float distance = Vector3.Distance(obj.transform.position, transform.position);
-                    if (distance < nearestDistance)
-                    {
-                        nearestObject = obj.transform;
-                        nearestDistance = distance;
-                    }
-                }
-
-                // Limita el zoom al rango establecido
-                float zoom = Mathf.Clamp(nearestDistance, maxZoom, minZoom);
-
-                // Imprime mensajes de depuración si se alcanzan los límites de zoom
-                if (nearestDistance < minZoom)
-                {
-                    Debug.Log("maxZoom");
-                }
-                else if (nearestDistance > maxZoom)
-                {
-                    Debug.Log("minZoom");
-                }
-
-                // Aplica un zoom suavizado a la cámara
+                float zoom = Mathf.Clamp(minZoom, minZoom, maxZoom);
                 cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoom, Time.deltaTime * zoomSpeed);
             }
         }
     }
 
     // Se llama cuando otro collider entra en el trigger.
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(targetTag))
         {
@@ -71,7 +47,7 @@ public class ZoomOutCam : MonoBehaviour
     }
 
     // Se llama cuando otro collider sale del trigger.
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag(targetTag))
         {
