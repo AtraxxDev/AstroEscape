b1c669c6f4a7d146c29ff579b4522a26a2e6917d
AtraxxDev <100314844+AtraxxDev@users.noreply.github.com>
2023-11-23 16:46:54 -0600

    Corrigiendo script max niveles

--- Assets/Scripts/seleccionNiveles.cs
+++ Assets/Scripts/seleccionNiveles.cs
@@ -4,51 +4,50 @@ using UnityEngine;
 
 public class seleccionNiveles : MonoBehaviour
 {
-
     public float velocidad = 5f;
-    public Transform[] puntos; 
-    private int indicePuntoActual = 0; 
+    public Transform[] puntos;
+    private int indicePuntoActual = 0;
+    private bool enMovimientoAutomatico = false;
 
     void Update()
     {
-        
         if (puntos.Length == 0)
         {
             Debug.LogWarning("No hay puntos especificados.");
             return;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        // Si no está en movimiento automático, verifica la entrada del jugador
+        if (!enMovimientoAutomatico)
         {
-            
+            // Avanzar al siguiente punto al presionar 'D'
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                indicePuntoActual = (indicePuntoActual + 1) % puntos.Length;
+                enMovimientoAutomatico = true;
+            }
+
+            // Retroceder al punto anterior al presionar 'A'
+            if (Input.GetKeyDown(KeyCode.A) && indicePuntoActual > 0)
+            {
+                indicePuntoActual--;
+                enMovimientoAutomatico = true;
+            }
+        }
+        else
+        {
+            // Si está en movimiento automático, mueve la nave hacia el punto actual
             Vector3 direccion = puntos[indicePuntoActual].position - transform.position;
             direccion.Normalize();
 
-            
             transform.Translate(direccion * velocidad * Time.deltaTime);
 
-            
+            // Verifica si la nave ha llegado al punto actual
             if (Vector3.Distance(transform.position, puntos[indicePuntoActual].position) < 0.1f)
             {
-                
-                indicePuntoActual = (indicePuntoActual + 1) % puntos.Length;
+                // Detén el movimiento automático y espera la entrada del jugador
+                enMovimientoAutomatico = false;
             }
         }
-        /*else if (Input.GetKey(KeyCode.A))
-        {
-            
-            Vector3 direccion = transform.position - puntos[indicePuntoActual].position;
-            direccion.Normalize();
-
-           
-            transform.Translate(direccion * velocidad * Time.deltaTime);
-
-            
-            if (Vector3.Distance(transform.position, puntos[indicePuntoActual].position) > 0.1f)
-            {
-                
-                indicePuntoActual = (puntos.Length + indicePuntoActual - 1) % puntos.Length;
-            }
-        }*/
     }
 }
