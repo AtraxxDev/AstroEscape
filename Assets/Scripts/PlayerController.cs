f98e8d7eb3dc4fa4506d50865c0a84c7555428d7
AtraxxDev <100314844+AtraxxDev@users.noreply.github.com>
2023-11-23 16:08:07 -0600

    Arreglando Rotacion jugador

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float rotationalSpeed = 0.5f;
+    public float maxAngularVelocity = 5f;  // Nueva variable para limitar la velocidad angular
     public float thrustForce = 0.5f;
     public float turboMultiplier = 2.0f;
     public float brakeForce = 0.5f;
@@ -13,30 +14,27 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-     
-        rb.gravityScale = 0f;
-   
-        rb.bodyType = RigidbodyType2D.Dynamic;
 
-        
+        rb.gravityScale = 0f;
+        rb.bodyType = RigidbodyType2D.Dynamic;
     }
 
     void Update()
     {
-      
         float rotationInput = Input.GetAxis("Horizontal");
         float thrustInput = Input.GetAxis("Vertical");
 
-       
-        float rotation = -rotationInput * rotationalSpeed; 
+        float rotation = -rotationInput * rotationalSpeed;
         rb.rotation += rotation;
+
+        // Limitar la velocidad angular
+        rb.angularVelocity = Mathf.Clamp(rb.angularVelocity, -maxAngularVelocity, maxAngularVelocity);
 
         Vector2 thrustDirection = transform.up;
         float thrust = thrustForce * thrustInput;
 
         if (thrustInput < 0)
         {
-            
             rb.AddForce(-thrustDirection * brakeForce);
         }
         else
@@ -44,10 +42,8 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(thrustDirection * thrust);
         }
 
-       
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
             rb.AddForce(thrustDirection * thrustForce * turboMultiplier, ForceMode2D.Impulse);
         }
     }
