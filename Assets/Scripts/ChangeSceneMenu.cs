ac4112b9ddffd4a5918b2809c3c1286b033baae0
Elliot Reyes <gametraxworks@gmail.com>
2023-11-30 12:38:24 -0600

    Agregando Sonidos y canvas

---
+++ Assets/Scripts/ChangeSceneMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneMenu : MonoBehaviour
+{
+    public AudioSource buttonPlay;
+    public AudioSource MusicBG;
+    public void StartGame()
+    {
+        StartCoroutine(StartG());
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("MenuP");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator StartG()
+    {
+        MusicBG.Stop();
+
+        if (buttonPlay != null) 
+        {
+            buttonPlay.Play();
+        }
+        
+        yield return new WaitForSeconds(buttonPlay.clip.length);
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+}
